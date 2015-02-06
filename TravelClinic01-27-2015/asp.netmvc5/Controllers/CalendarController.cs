fee517ac5f84f6de73f2d901699f4b7ce76135d4
Woodman502 <joewoodcox@icloud.com>
2015-02-05 20:11:33 -0500

    Calendar fully operational

--- TravelClinic01-27-2015/asp.netmvc5/Controllers/CalendarController.cs
+++ TravelClinic01-27-2015/asp.netmvc5/Controllers/CalendarController.cs
@@ -15,54 +15,7 @@ namespace asp.netmvc5.Controllers
             return View();
         }
 
-        public ActionResult GetEvents(double start, double end)
-        {
-            var fromDate = ConvertFromUnixTimestamp(start);
-            var toDate = ConvertFromUnixTimestamp(end);
-
-            //Get the events
-            //You may get from the repository also
-            var eventList = GetEvents();
-
-            var rows = eventList.ToArray();
-            return Json(rows, JsonRequestBehavior.AllowGet);
-        }
-
-        private List<Events> GetEvents()
-        {
-            List<Events> eventList = new List<Events>();
-
-            Events newEvent = new Events
-            {
-                id = "1",
-                title = "Event 1",
-                start = DateTime.Now.AddDays(1).ToString("s"),
-                end = DateTime.Now.AddDays(1).ToString("s"),
-                allDay = false
-            };
-
-
-            eventList.Add(newEvent);
-
-            newEvent = new Events
-            {
-                id = "1",
-                title = "Event 3",
-                start = DateTime.Now.AddDays(2).ToString("s"),
-                end = DateTime.Now.AddDays(3).ToString("s"),
-                allDay = false
-            };
-
-            eventList.Add(newEvent);
-
-            return eventList;
-        }
-
-        private static DateTime ConvertFromUnixTimestamp(double timestamp)
-        {
-            var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            return origin.AddSeconds(timestamp);
-        }
+        
 
     }
 
