c687b4281a81fb7b00146291332ffaf05c26e92a
NickMattmiller <njmatt03@louisville.edu>
2015-02-12 16:29:15 -0500

    Iteration 1 Final

--- aspnetmvc5/Calendar.sql
+++
@@ -1,7 +0,0 @@
-﻿Create Table Events (
-[id] int IDENTITY (1,1) not null,
-[text] nvarchar(250) null,
-[start_date] datetime not null,
-[end_date] datetime not null,
-Primary Key (id)
-)
--- aspnetmvc5/asp.netmvc5/Microsoft Templates/Calendar/Calendar/Default.cshtml
+++
@@ -1,1 +0,0 @@
-﻿@*Content Rendered by Theme*@
