c7713850f57e996a0a3aa8803acdf2dd99c359cb
Brett Leger <goku759@gmail.com>
2015-03-02 15:39:24 -0500

    added file manager class to picture.cs for future scaffolding to a DB

--- TravelClinic/Models/Picture.cs
+++ TravelClinic/Models/Picture.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -8,5 +10,19 @@ namespace asp.netmvc5.Models
     public class Picture
     {
         public IEnumerable<HttpPostedFileBase> Files { get; set; }
+    }
+ 
+    public class FileManager
+    {
+        [Required]
+        public int FileId { get; set; }
+        [Required]
+        public string FileName { get; set; }
+        public string Type { get; set; }
+        public decimal FileSize { get; set; }
+    }
+    public class FileManagerDBContext : DbContext
+    {
+        public DbSet<FileManager> Files { get; set; }
     }
 }
