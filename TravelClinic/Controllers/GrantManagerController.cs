c650a9bc1dc36f9479aa7e9306d558437ad01591
Brett Leger <goku759@gmail.com>
2015-03-02 19:47:59 -0500

    new grant manager template design

---
+++ TravelClinic/Controllers/GrantManagerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asp.netmvc5.Models;
+
+namespace asp.netmvc5.Controllers
+{
+    public class GrantManagerController : Controller
+    {
+        // GET: GrantManager
+        [HttpGet]
+        public ActionResult GrantManager()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GrantManager(GrantManagerModel model)
+        {
+            foreach (string upload in Request.Files)
+            {
+                var uploadreq = Request.Files[upload];
+                if (uploadreq != null && uploadreq.ContentLength == 0) continue;
+                string pathToSave = Server.MapPath("~/Files");
+                if (uploadreq != null)
+                {
+                    string filename = Path.GetFileName(uploadreq.FileName);
+                    if (filename != null) uploadreq.SaveAs(Path.Combine(pathToSave, filename));
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ TravelClinic/Models/GrantManagerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace asp.netmvc5.Models
+{
+    public class GrantManagerModel
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name= "Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+    }
+}
