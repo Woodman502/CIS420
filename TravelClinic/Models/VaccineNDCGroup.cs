8b2e305a125e7b0e88e64660b15b0e9259061d71
NickMattmiller <njmatt03@louisville.edu>
2015-03-05 20:50:05 -0500

    Grants working. #2

---
+++ TravelClinic/Models/VaccineNDCGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace asp.netmvc5.Models
+{
+    public class VaccineNDCGroup
+    {
+        public Int64? Barcode_NDC { get; set; }
+        public int VaccineCount { get; set; }
+        public string Description { get; set; }
+
+
+    }
+
+}
